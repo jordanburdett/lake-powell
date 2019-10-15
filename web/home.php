bc47d15dc8912e79a42900c8c761fd9327e52026
Jordan <jordan@burdett.us>
2019-10-15 11:02:11 -0600

    built home.php index

--- web/home.php
+++ web/home.php
@@ -6,8 +6,8 @@ session_start();
 <html>
 
 <head>
-    <title></title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Lake Powell Share Holder</title>
+    <link rel="stylesheet" href="web/styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
