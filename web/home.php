e20b0b903b701fe1f320a444b50a6143e2a916ef
Jordan <jordan@burdett.us>
2019-10-15 11:33:54 -0600

    built home.php index

--- web/home.php
+++ web/home.php
@@ -22,7 +22,7 @@ session_start();
             Lake Powell
         </h1>
 
-        <nav class="navbar bg-dark navbar-expand-sm navbar-transparent>
+        <nav class="navbar navbar-expand-sm navbar-transparent>
 
             <!-- bootstrap navbar -->
             <ul class="navbar-nav">
