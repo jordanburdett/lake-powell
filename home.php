b25c5003b50302840e1bcd4e810dc18447d59b8f
Jordan <jordan@burdett.us>
2019-10-15 14:33:49 -0600

    built home.php index

--- home.php
+++ home.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+
+
 ?>
 
 <!DOCTYPE html>
@@ -23,22 +25,32 @@ session_start();
         </h1>
 
         <nav class="navbar navbar-expand-sm navbar-transparent">
-
             <!-- bootstrap navbar -->
 
-            <ul class= "navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link" href="home.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="info.php">Info</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link 3</a>
-            </li>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="info.php">Info</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link 3</a>
+                </li>
             </ul>
 
         </nav>
+    </div>
+
+    <div class="float">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Card title</h4>
+                <p class="card-text">Some example text. Some example text.</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
     </div>
 
 
