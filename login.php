24293294a5886354639a9b7736ca8f109a090ceb
Jordan <jordan@burdett.us>
2019-10-18 18:35:32 -0600

    login

--- login.php
+++ login.php
@@ -1,28 +1,26 @@
 <?php
 session_start();
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
 
+  $dbOpts = parse_url($dbUrl);
 
-try {
-$dbUrl = getenv('DATABASE_URL');
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
 
-$dbOpts = parse_url($dbUrl);
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-$dbHost = $dbOpts["host"];
-$dbPort = $dbOpts["port"];
-$dbUser = $dbOpts["user"];
-$dbPassword = $dbOpts["pass"];
-$dbName = ltrim($dbOpts["path"],'/');
-
-$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $ex)
 {
-echo 'Error!: ' . $ex->getMessage();
-die();
+  echo 'Error!: ' . $ex->getMessage();
+  die();
 }
-
 
 
 
@@ -31,14 +29,15 @@ $password = $_POST['password'];
 $first_name = "";
 $last_name = "";
 
+/*
 foreach($db->query('SELECT username, password, first_name, last_name FROM user_profile
-                    WHERE username = ' . $username .' AND password = ' . $password) as $row) 
+                    WHERE username = ' . $username . 'AND password = ' . $password) as $row) 
                     { 
                         echo '<p>' . $row['username'] . ' , ' . $row['password'] . "</p>";
                         $first_name = $row['first_name'];
                         $last_name = $row['last_name'];
                     }
-
+*/
 
 if ($first_name == "") {
     echo "invalid login";
