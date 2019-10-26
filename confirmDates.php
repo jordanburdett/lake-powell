2aa2984b996ad89aeb8ffda09dbefdc316f366cf
Jordan <jordan@burdett.us>
2019-10-26 16:16:17 -0600

    test

--- confirmDates.php
+++ confirmDates.php
@@ -1,29 +1,26 @@
 <?php
-
 session_start();
 
-if ($_SESSION['loggedIn'] != true) {
-    echo "You do not have access";
-    die();
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-
-try {
-    $dbUrl = getenv('DATABASE_URL');
-
-    $dbOpts = parse_url($dbUrl);
-
-    $dbHost = $dbOpts["host"];
-    $dbPort = $dbOpts["port"];
-    $dbUser = $dbOpts["user"];
-    $dbPassword = $dbOpts["pass"];
-    $dbName = ltrim($dbOpts["path"], '/');
-
-    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $ex) {
-    echo 'Error!: ' . $ex->getMessage();
-    die();
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
 }
 
 
@@ -34,6 +31,8 @@ $year = $_POST['year'];
 $month = $_POST['month'];
 $user_id = $_SESSION['user_id'];
 $info = $_POST['info'];
+
+echo "$dayStart, $dayEnd, $year, $month, $user_id, $info";
 
 $statement = $db->prepare("INSERT INTO dates(user_id, month_start, month_end, day_start, day_end, year_start, year_end)
 VALUES ('$user_id', '$month', '$month', '$dayStart', '$dayEnd', '$year', '$year')");
