7d378a2ba636d9166002c62283d6df9dd9ffb577
Jordan <jordan@burdett.us>
2019-10-26 16:11:19 -0600

    test

--- confirmDates.php
+++ confirmDates.php
@@ -35,8 +35,8 @@ $month = $_POST['month'];
 $user_id = $_SESSION['user_id'];
 $info = $_POST['info'];
 
-//$statement = $db->prepare("INSERT INTO dates(user_id, month_start, month_end, day_start, day_end, year_start, year_end)
-//VALUES ($user_id, $month, $month, $dayStart, $dayEnd, $year, $year)");
+$statement = $db->prepare("INSERT INTO dates(user_id, month_start, month_end, day_start, day_end, year_start, year_end)
+VALUES ($user_id, $month, $month, $dayStart, $dayEnd, $year, $year)");
 
 //$statement->execute();
 
