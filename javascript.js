ea8fc47de1b4cf59586806797634f8b05389d513
Jordan <jordan@burdett.us>
2019-10-26 14:26:50 -0600

    test

--- javascript.js
+++ javascript.js
@@ -63,6 +63,8 @@ function reserve(){
     console.log(year);
     console.log(month);
     console.log(info);
+    console.log(daysSelected[0]);
+    console.log(daysSelected[daysSelected.length - 1]);
 
     $('confirm').load('login.php', { 'dayStart': daysSelected[0], 'dayEnd': daysSelected[daysSelected.length - 1]
                       , 'year' : year, 'month': month, 'info': info});
