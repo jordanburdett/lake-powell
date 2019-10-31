73e67e9159664767cd4eba931cb7c9a2d4d77a17
Jordan Burdett <jordan@burdett.us>
2019-10-31 16:59:56 -0600

    test

--- login.php
+++ login.php
@@ -41,50 +41,13 @@ foreach($db->query('SELECT username, password, first_name, last_name, id FROM us
 
 
 if ($first_name == "") {
-    echo "<ul class='nav nav-tabs nav-justified'>
-    <li class='nav-item'>
-        <a class='nav-link' href='home.php'>Home</a>
-    </li>
-    <li class='nav-item '>
-        <a class='nav-link active' href='loginPage.php'>Login</a>
-        </li>
-    </ul>";
-    echo "<p style='color:red'>invalid login</p>";
-    echo "<div class='row'>
-    <div class='col'>
-        <h2 class='smallHeader'>Login</h2>
-
-        <form name='loginForm' id='loginForm'>
-            <div class='form-group'>
-                <label for='username'>Username:</label>
-                <input type='txt' class='form-control' id='username' name='username'>
-            </div>
-            <div class='form-group'>
-                <label for='password'>Password:</label>
-                <input type='password' name='password' class='form-control' id='password'>
-            </div>
-
-            <input type=submit class='btn btn-primary' onclick='login()'>Login</button>
-        </form>
-    </div>
-
-</div>";
+    
+    header("location: loginPage.php");
 }
 else
 {
 
-    echo "<ul class='nav nav-tabs nav-justified'>
-    <li class='nav-item'>
-        <a class='nav-link' href='home.php'>Home</a>
-    </li>
-    <li class='nav-item'>
-        <a class='nav-link' href='info.php'>Account Info</a>
-    </li>
-    <li class='nav-item'>
-        <a class='nav-link' href='schedule.php'>Schedule</a>
-    </li>
-    </ul>";
-    echo "<h4 style='text-align:center; padding: 20px;'>Welcome " . $first_name . " " . $last_name . " you are now logged in</h4>";
+    
     
 
     $_SESSION['loggedIn']   = true;
@@ -92,5 +55,7 @@ else
     $_SESSION['last_name']  = $last_name;
     $_SESSION['user_id']    = $user_id;
     $_SESSION['username']   = $username;
+
+    header("location: home.php");
 }
 ?>
